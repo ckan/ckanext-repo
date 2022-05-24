51e9aeb6306965c4ecbe88291a80a733c6a6728c
amercader <amercadero@gmail.com>
2022-05-24 14:35:11 +0200

    Cleanup

--- ckanext/repo/plugin.py
+++ ckanext/repo/plugin.py
@@ -19,14 +19,14 @@ class RepoInfo(p.SingletonPlugin):
 
     def update_config(self, config):
         # add template directory
-        p.toolkit.add_template_directory(config, 'templates')
+        p.toolkit.add_template_directory(config, "templates")
 
         self.repos_info = []
 
-        src_path = config.get('ckanext.repo.srcpath', False)
+        src_path = config.get("ckanext.repo.srcpath", False)
 
-        repo_refs = config.get('ckanext.repo.repos', 'ckan')
-        repo_refs = repo_refs.split(' ')
+        repo_refs = config.get("ckanext.repo.repos", "ckan")
+        repo_refs = repo_refs.split(" ")
 
         for repo_ref in repo_refs:
             repo_info = _get_repo_info(repo_ref, src_path)
@@ -37,8 +37,8 @@ class RepoInfo(p.SingletonPlugin):
 
     def get_helpers(self):
         return {
-            'get_repos_info': self._get_repos_info,
-            'render_repos_info': self._render_repos_info,
+            "get_repos_info": self._get_repos_info,
+            "render_repos_info": self._render_repos_info,
         }
 
     # IActions
@@ -50,21 +50,21 @@ class RepoInfo(p.SingletonPlugin):
             return self.repos_info
 
         return {
-            'repos_info_show': repos_info_show,
+            "repos_info_show": repos_info_show,
         }
 
     def _get_repos_info(self):
         return self.repos_info
 
-
     def _render_repos_info(self):
-        return p.toolkit.render_snippet('repo_snippet.html',
-                                        {'repos_info': self.repos_info})
+        return p.toolkit.render_snippet(
+            "repo_snippet.html", {"repos_info": self.repos_info}
+        )
 
 
 def _import_ckanext_module(name):
     try:
-        return getattr(__import__('ckanext', fromlist=[name]), name)
+        return getattr(__import__("ckanext", fromlist=[name]), name)
     except ImportError:
         return None
 
@@ -78,24 +78,24 @@ def _import_module(name):
 
 def _get_repo_info(repo_ref, src_path=False):
 
-    if '/' in repo_ref:
-        parts = repo_ref.split('/')
+    if "/" in repo_ref:
+        parts = repo_ref.split("/")
         github_org = parts[0]
         repo_name = parts[1]
     else:
-        github_org = 'ckan'
+        github_org = "ckan"
         repo_name = repo_ref
 
-    if repo_name.startswith('ckanext-'):
-        parts = repo_name.split('-')
+    if repo_name.startswith("ckanext-"):
+        parts = repo_name.split("-")
         module = _import_ckanext_module(parts[1])
-        cwd_dir = '../..'
+        cwd_dir = "../.."
     else:
         module = _import_module(repo_name)
-        cwd_dir = '..'
+        cwd_dir = ".."
 
     if not module:
-        log.error('Could not load module for repo {0}'.format(repo_ref))
+        log.error("Could not load module for repo {0}".format(repo_ref))
         return None
 
     if src_path:
@@ -104,31 +104,36 @@ def _get_repo_info(repo_ref, src_path=False):
         cwd = os.path.join(os.path.dirname(module.__file__), cwd_dir)
 
     repo_info = {
-        'name': repo_name,
-        'hash': '',
-        'title': '',
-        'url': '',
-        'branch': '',
+        "name": repo_name,
+        "hash": "",
+        "title": "",
+        "url": "",
+        "branch": "",
     }
 
     # Current commit
     try:
         git_show = subprocess.check_output(
-            ['git', 'show', '--oneline'], cwd=cwd).decode('utf-8')
-        commit = git_show.split('\n')[0].split(' ')
-        repo_info['hash'] = commit.pop(0)
-        repo_info['title'] = ' '.join(commit)
-        repo_info['url'] = 'https://github.com/{0}/{1}/commit/{2}'.format(
-            github_org,
-            repo_name,
-            repo_info['hash'])
+            ["git", "show", "--oneline"], cwd=cwd
+        ).decode("utf-8")
+        commit = git_show.split("\n")[0].split(" ")
+        repo_info["hash"] = commit.pop(0)
+        repo_info["title"] = " ".join(commit)
+        repo_info["url"] = "https://github.com/{0}/{1}/commit/{2}".format(
+            github_org, repo_name, repo_info["hash"]
+        )
     except subprocess.CalledProcessError:
         pass
 
     # Current branch (requires Git 1.6.3 or higher)
     try:
-        repo_info['branch'] = subprocess.check_output(
-            ['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=cwd).decode('utf-8').strip()
+        repo_info["branch"] = (
+            subprocess.check_output(
+                ["git", "rev-parse", "--abbrev-ref", "HEAD"], cwd=cwd
+            )
+            .decode("utf-8")
+            .strip()
+        )
 
     except subprocess.CalledProcessError:
         pass
@@ -136,9 +141,11 @@ def _get_repo_info(repo_ref, src_path=False):
     # Last pulled
     try:
         date = subprocess.check_output(
-            ['stat', '-c', '%Y', '.git/FETCH_HEAD'], cwd=cwd).decode('utf-8')
-        repo_info['last_updated'] = datetime.datetime.fromtimestamp(
-            int(date)).strftime('%Y-%m-%d %H:%M:%S')
+            ["stat", "-c", "%Y", ".git/FETCH_HEAD"], cwd=cwd
+        ).decode("utf-8")
+        repo_info["last_updated"] = datetime.datetime.fromtimestamp(int(date)).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
     except subprocess.CalledProcessError:
         pass
 
--- setup.py
+++ setup.py
@@ -1,31 +1,25 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.0'
+version = "0.0"
 
 setup(
-	name='ckanext-repo',
-	version=version,
-	description='Shows basic information about the CKAN version an instance is running',
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Open Knowledge Foundation',
-	author_email='',
-	url='http://okfn.org',
-	license='AGPL',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.repo'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    name="ckanext-repo",
+    version=version,
+    description="Shows basic information about the CKAN version an instance is running",
+    keywords="",
+    author="Open Knowledge Foundation",
+    author_email="",
+    url="http://okfn.org",
+    license="AGPL",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    namespace_packages=["ckanext", "ckanext.repo"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
         [ckan.plugins]
-	# Add plugins here, eg
-	repo_info=ckanext.repo.plugin:RepoInfo
-	""",
+        repo_info=ckanext.repo.plugin:RepoInfo
+    """,
 )
